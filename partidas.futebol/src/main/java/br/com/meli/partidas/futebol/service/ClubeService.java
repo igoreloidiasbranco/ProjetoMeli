61294d6b0fc6a5351ea3d129b7f657816a2fd9f6
igoreloidiasbranco <igoreloi1981@gmail.com>
2025-07-01 16:01:23 -0300

    Criação da interface ClubeService

---
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/service/ClubeService.java
@@ -0,0 +1,8 @@
+package br.com.meli.partidas.futebol.service;
+
+import br.com.meli.partidas.futebol.entity.Clube;
+
+public interface ClubeService {
+
+    public Clube salvarClube(Clube clube);
+}
