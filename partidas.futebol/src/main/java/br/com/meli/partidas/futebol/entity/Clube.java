d95e7936337be8f01dc516e78ac71e81a70f288a
igoreloidiasbranco <igoreloi1981@gmail.com>
2025-07-01 11:59:06 -0300

    Criação da classe Clube

---
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/entity/Clube.java
@@ -0,0 +1,67 @@
+package br.com.meli.partidas.futebol.entity;
+
+import br.com.meli.partidas.futebol.dto.Sigla;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clubes")
+public class Clube {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    private Sigla sigla;
+
+    @JsonProperty("data_criacao")
+    LocalDate dataCriacao;
+
+    Boolean ativo;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Sigla getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(Sigla sigla) {
+        this.sigla = sigla;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
