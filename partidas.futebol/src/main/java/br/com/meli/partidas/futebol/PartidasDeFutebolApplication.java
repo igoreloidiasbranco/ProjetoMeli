0f3f1c7aab93901271b1ed9ffdb962395729bbf1
igoreloidiasbranco <igoreloi1981@gmail.com>
2025-07-01 09:29:26 -0300

    primeiro commit

---
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/PartidasDeFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.partidas.futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidasDeFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidasDeFutebolApplication.class, args);
+	}
+
+}
