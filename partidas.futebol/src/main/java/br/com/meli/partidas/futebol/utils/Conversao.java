81cb7275a72cbe87b11baa3ba3d28d7d81fe371e
igoreloidiasbranco <igoreloi1981@gmail.com>
2025-07-07 18:44:33 -0300

    add Conversao para Estadio

--- partidas.futebol/src/main/java/br/com/meli/partidas/futebol/utils/Conversao.java
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/utils/Conversao.java
@@ -1,11 +1,17 @@
 package br.com.meli.partidas.futebol.utils;
 
 import br.com.meli.partidas.futebol.dto.request.ClubeRequestDTO;
+import br.com.meli.partidas.futebol.dto.request.EstadioRequestDTO;
 import br.com.meli.partidas.futebol.dto.response.ClubeResponseDTO;
+import br.com.meli.partidas.futebol.dto.response.EstadioResponseDTO;
 import br.com.meli.partidas.futebol.entity.Clube;
+import br.com.meli.partidas.futebol.entity.Estadio;
 import org.springframework.beans.BeanUtils;
 
 public class Conversao {
+
+    private Conversao() {
+    }
 
     public static Clube dtoToEntity(ClubeRequestDTO clubeRequestDTO) {
         Clube clube = new Clube();
@@ -19,5 +25,16 @@ public class Conversao {
         return clubeResponseDTO;
     }
 
+    public static Estadio  dtoToEntity(EstadioRequestDTO estadioRequestDTO) {
+        Estadio estadio = new Estadio();
+        BeanUtils.copyProperties(estadioRequestDTO, estadio);
+        return estadio;
+    }
+
+    public static EstadioResponseDTO entityToDTO(Estadio estadio) {
+        EstadioResponseDTO estadioResponseDTO = new EstadioResponseDTO();
+        BeanUtils.copyProperties(estadio, estadioResponseDTO);
+        return estadioResponseDTO;
+    }
 
 }
