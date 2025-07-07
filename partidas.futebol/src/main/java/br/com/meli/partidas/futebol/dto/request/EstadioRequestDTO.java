e8b921b6f635cfd621c13d6c2ed1c8319fe2583c
igoreloidiasbranco <igoreloi1981@gmail.com>
2025-07-07 18:48:02 -0300

    add classe EstadioRequestDTO

---
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/dto/request/EstadioRequestDTO.java
@@ -0,0 +1,33 @@
+package br.com.meli.partidas.futebol.dto.request;
+
+import br.com.meli.partidas.futebol.dto.Sigla;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class EstadioRequestDTO {
+
+    @NotBlank
+    @Pattern(regexp = "^.{3,}$")
+    private String nome;
+
+    @NotNull
+    private Sigla sigla;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Sigla getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(Sigla sigla) {
+        this.sigla = sigla;
+    }
+}
