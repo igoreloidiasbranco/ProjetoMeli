d1f5245744dd998a61373d2a5ca78fbfbc60180d
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-08 18:20:37 -0300

    add findByNome

--- partidas.futebol/src/main/java/br/com/meli/partidas/futebol/repository/EstadioRepository.java
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/repository/EstadioRepository.java
@@ -1,9 +1,13 @@
 package br.com.meli.partidas.futebol.repository;
 
 import br.com.meli.partidas.futebol.entity.Estadio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EstadioRepository extends JpaRepository<Estadio, Long> {
 
     boolean existsByNome(String nome);
+
+    Page<Estadio> findByNome(String nome, Pageable paginacao);
 }
