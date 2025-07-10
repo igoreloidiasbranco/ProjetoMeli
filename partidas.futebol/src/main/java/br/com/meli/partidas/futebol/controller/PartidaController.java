86d43136ce115fff2fbe9ed50ef7378320c4bb49
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-10 18:29:20 -0300

    add classe PartidaController

---
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/controller/PartidaController.java
@@ -0,0 +1,11 @@
+package br.com.meli.partidas.futebol.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/partidas")
+public class PartidaController {
+
+
+}
