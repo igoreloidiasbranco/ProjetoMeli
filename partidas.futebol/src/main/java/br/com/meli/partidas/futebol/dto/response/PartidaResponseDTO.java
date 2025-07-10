74ee6f326410656bf548372fd14be3862f7ab312
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-10 18:30:12 -0300

    add classe PartidaResponseDTO

---
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/dto/response/PartidaResponseDTO.java
@@ -0,0 +1,67 @@
+package br.com.meli.partidas.futebol.dto.response;
+
+import java.time.LocalDateTime;
+
+public class PartidaResponseDTO {
+
+    private Long id;
+
+    private Long idClubeMandante;
+
+    private Long idClubeVisitante;
+
+    private String resultado;
+
+    private Long idEstadio;
+
+    private LocalDateTime dataHoraPartida;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdClubeMandante() {
+        return idClubeMandante;
+    }
+
+    public void setIdClubeMandante(Long idClubeMandante) {
+        this.idClubeMandante = idClubeMandante;
+    }
+
+    public Long getIdClubeVisitante() {
+        return idClubeVisitante;
+    }
+
+    public void setIdClubeVisitante(Long idClubeVisitante) {
+        this.idClubeVisitante = idClubeVisitante;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+
+    public Long getIdEstadio() {
+        return idEstadio;
+    }
+
+    public void setIdEstadio(Long idEstadio) {
+        this.idEstadio = idEstadio;
+    }
+
+    public LocalDateTime getDataHoraPartida() {
+        return dataHoraPartida;
+    }
+
+    public void setDataHoraPartida(LocalDateTime dataHoraPartida) {
+        this.dataHoraPartida = dataHoraPartida;
+    }
+}
