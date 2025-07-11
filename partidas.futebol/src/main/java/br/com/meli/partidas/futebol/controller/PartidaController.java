7bd45101540b6f78efeb7476bb46967722d5c1e6
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-11 18:31:39 -0300

    add salvarPartida

--- partidas.futebol/src/main/java/br/com/meli/partidas/futebol/controller/PartidaController.java
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/controller/PartidaController.java
@@ -1,11 +1,29 @@
 package br.com.meli.partidas.futebol.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.meli.partidas.futebol.dto.request.PartidaRequestDTO;
+import br.com.meli.partidas.futebol.dto.response.PartidaResponseDTO;
+import br.com.meli.partidas.futebol.entity.Partida;
+import br.com.meli.partidas.futebol.service.PartidaService;
+import br.com.meli.partidas.futebol.utils.Conversao;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/partidas")
 public class PartidaController {
 
+    private final PartidaService partidaService;
+
+    public PartidaController(PartidaService partidaService) {
+        this.partidaService = partidaService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PartidaResponseDTO salvarPartida(@RequestBody @Valid PartidaRequestDTO partidaRequestDTO) {
+        Partida partida = partidaService.salvarPartida(partidaRequestDTO);
+        return Conversao.entityToDTO(partida);
+    }
 
 }
