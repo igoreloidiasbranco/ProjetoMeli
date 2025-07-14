06e9aa751181a998c2048a7e66793d69f0369e5f
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-14 12:40:07 -0300

    add isClubesAtivos buscarEstadio e removido criarPartida

--- partidas.futebol/src/main/java/br/com/meli/partidas/futebol/service/PartidaService.java
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/service/PartidaService.java
@@ -1,15 +1,20 @@
 package br.com.meli.partidas.futebol.service;
 
 import br.com.meli.partidas.futebol.dto.request.PartidaRequestDTO;
+import br.com.meli.partidas.futebol.entity.Clube;
+import br.com.meli.partidas.futebol.entity.Estadio;
 import br.com.meli.partidas.futebol.entity.Partida;
+
+import java.time.LocalDate;
 
 public interface PartidaService {
 
-    void isTimesIguais(PartidaRequestDTO partidaRequestDTO);
-    void isGolsNegativos(Integer golsMandante, Integer golsVisitante);
-    void isDataHoraAntesCriacaoClube(PartidaRequestDTO partidaRequestDTO);
     void isClubesExistem(PartidaRequestDTO partidaRequestDTO);
-    Partida criarPartida(PartidaRequestDTO partidaRequestDTO);
+    void isClubesDiferentes(PartidaRequestDTO partidaRequestDTO);
+    void isGolsPositivos(Integer golsMandante, Integer golsVisitante);
+    void isDataHoraAntesCriacaoClube(Clube clubeMandante, Clube clubeVisitante, LocalDate dataHoraPartida);
+    void isClubesAtivos(Clube clubeMandante, Clube clubeVisitante);
+    Estadio buscarEstadio(Long idEstadio);
     Partida salvarPartida(PartidaRequestDTO partidaRequestDTO);
 
 }
