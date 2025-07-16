552ceee1219b1e1acb565d87e29446b24215aef8
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-16 18:25:14 -0300

    renomeado obj para clubeEditado

--- partidas.futebol/src/main/java/br/com/meli/partidas/futebol/repository/ClubeRepository.java
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/repository/ClubeRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClubeRepository extends JpaRepository<Clube, Long> {
 
     boolean existsByNomeAndSigla(String nome, Sigla sigla);
+    Clube findByNomeAndSigla(String nome, Sigla sigla);
     Page<Clube> findByNome(String nome, Pageable paginacao);
     Page<Clube> findBySigla(Sigla sigla, Pageable paginacao);
     Page<Clube> findByAtivo(Boolean ativo, Pageable paginacao);
