586281c0903994c55832b300cd63645b9786b269
Igor Eloi <igor.dbranco@mercadolivre.com>
2025-07-25 18:49:33 -0300

    removido existsByNomeAndSigla

--- partidas.futebol/src/main/java/br/com/meli/partidas/futebol/repository/ClubeRepository.java
+++ partidas.futebol/src/main/java/br/com/meli/partidas/futebol/repository/ClubeRepository.java
@@ -6,11 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ClubeRepository extends JpaRepository<Clube, Long> {
 
-    boolean existsByNomeAndSigla(String nome, Sigla sigla);
     Clube findByNomeAndSigla(String nome, Sigla sigla);
     Page<Clube> findByNome(String nome, Pageable paginacao);
     Page<Clube> findBySigla(Sigla sigla, Pageable paginacao);
